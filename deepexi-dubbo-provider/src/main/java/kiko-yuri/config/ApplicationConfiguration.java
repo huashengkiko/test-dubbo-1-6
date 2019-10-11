01377c4875ffac004a35509033909e853237a2a6
test <test@qq.com>
2019-10-11 15:07:36 +0800

    source code push

---
+++ deepexi-dubbo-provider/src/main/java/kiko-yuri/config/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package kiko-yuri.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+    
+    
+
+}
+
